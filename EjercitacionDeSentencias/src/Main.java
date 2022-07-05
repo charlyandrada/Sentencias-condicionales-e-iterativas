4f0ec81638dc3071dba214032c51ca716bf21052
carlos <carlos_andrada21191@hotmail.com>
2022-07-04 22:05:54 -0300

    cambios primer ejercicio

--- EjercitacionDeSentencias/src/Main.java
+++ EjercitacionDeSentencias/src/Main.java
@@ -1,5 +1,26 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        //Crear un programa que pida dos numeros por consola y me indique cual es el mayor.
+
+        Scanner myScanner = new Scanner(System.in);
+        System.out.println("Ingresar el primer numero:");
+        int n1 , n2 ;
+        n1 = myScanner.nextInt();
+        System.out.println("Ingresar el segundo numero:");
+        n2 = myScanner.nextInt();
+
+        if (n1 == n2) {
+            System.out.println("Son iguales");
+        } else if (n1 < n2 ) {
+            System.out.println("El numero mayor es:" + n2);
+        }
+        else {
+            System.out.println("El numero mayor es:" + n1);
+        }
+
+
     }
 }
