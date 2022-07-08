3639523e06ac5ee66b5683274c9ebd0b9b19cab1
carlos <carlos_andrada21191@hotmail.com>
2022-07-08 20:55:30 -0300

    cambio a ejercicio4 terminadp

--- EjercitacionDeSentencias/src/Main.java
+++ EjercitacionDeSentencias/src/Main.java
@@ -1,5 +1,9 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        // imprimir por consola 10 veces el string foo//
+
+        for (int i = 0; i < 10; i++) {
+            System.out.println("foo");
+        }
     }
 }
