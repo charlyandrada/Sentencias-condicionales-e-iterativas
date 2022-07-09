aac32148213ef06287338d78008d66f900004dd0
carlos <carlos_andrada21191@hotmail.com>
2022-07-09 12:00:27 -0300

    cambio a metodo ejercicio5

--- EjercitacionDeSentencias/src/Main.java
+++ EjercitacionDeSentencias/src/Main.java
@@ -1,5 +1,9 @@
 public class Main {
     public static void main(String[] args) {
+        exercise5();
+    }
+
+    private static void exercise5() {
         // imprimir por consola los numeros del 1 al 10, uno por linea//
 
 
