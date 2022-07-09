76eeed81d1cc423c584ef7ba32fed8074e96ee7f
carlos <carlos_andrada21191@hotmail.com>
2022-07-09 12:09:16 -0300

    cambio a metodo ejercicio7

--- EjercitacionDeSentencias/src/Main.java
+++ EjercitacionDeSentencias/src/Main.java
@@ -1,5 +1,9 @@
 public class Main {
     public static void main(String[] args) {
+        exercise7();
+    }
+
+    private static void exercise7() {
         //imprimir triangulo de mayor a menor//
         for (int i = 0; i < 7; i++) {
             String msg = "";
