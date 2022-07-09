e7d894d1bffd7eb3ee9c4737d73b7aee002508be
carlos <carlos_andrada21191@hotmail.com>
2022-07-08 21:13:20 -0300

    cambios en ejercicio5

--- EjercitacionDeSentencias/src/Main.java
+++ EjercitacionDeSentencias/src/Main.java
@@ -1,5 +1,11 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        // imprimir por consola los numeros del 1 al 10, uno por linea//
+
+
+        for (int i = 1; i < 11; i++) {
+            System.out.println(i);
+
+        }
     }
 }
