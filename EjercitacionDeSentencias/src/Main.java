e2318cc90797e9dcab1f3b4937cec93275690b1b
carlos <carlos_andrada21191@hotmail.com>
2022-07-09 12:10:59 -0300

    cambio a metodo ejercicio8

--- EjercitacionDeSentencias/src/Main.java
+++ EjercitacionDeSentencias/src/Main.java
@@ -1,5 +1,9 @@
 public class Main {
     public static void main(String[] args) {
+        exercise8();
+    }
+
+    private static void exercise8() {
         for (int i = 0; i < 7; i++) {
             String msg = "";
             for (int j = 0; j < 6 - i; j++) {
