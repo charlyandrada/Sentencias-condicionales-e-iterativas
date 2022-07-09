3e7750b9f06aa8c64b37b3e3bda930380b1389e2
carlos <carlos_andrada21191@hotmail.com>
2022-07-09 11:58:15 -0300

    cambio a metodo ejercicio4

--- EjercitacionDeSentencias/src/Main.java
+++ EjercitacionDeSentencias/src/Main.java
@@ -1,5 +1,9 @@
 public class Main {
     public static void main(String[] args) {
+        exercise4();
+    }
+
+    private static void exercise4() {
         // imprimir por consola 10 veces el string foo//
 
         for (int i = 0; i < 10; i++) {
