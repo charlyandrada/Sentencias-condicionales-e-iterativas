77dc8dab6c0d351d501a347efa9df196a08ccda1
carlos <carlos_andrada21191@hotmail.com>
2022-07-12 01:27:21 -0300

    cambio ejercicio2

--- EjercitacionDeSentencias/src/Main.java
+++ EjercitacionDeSentencias/src/Main.java
@@ -4,7 +4,6 @@ public class Main {
     public static void main(String[] args) {
         exercise2();
 
-
     }
 
     private static void exercise2() {
@@ -43,27 +42,5 @@ public class Main {
             System.out.println("10% de recargo en su compra: tarjetas aderidas a compras visa mastercar, naranja visa");
         }
         System.out.println("El precio final a pagar por el producto " + producto + " es: $ " + totalapagar);
-
-
-        exercise1();
-
-    }
-
-    private static void exercise1() {
-        Scanner myScanner = new Scanner(System.in);
-        System.out.println("Ingresar el primer numero:");
-        int n1 , n2 ;
-        n1 = myScanner.nextInt();
-        System.out.println("Ingresar el segundo numero:");
-        n2 = myScanner.nextInt();
-
-        if (n1 == n2) {
-            System.out.println("Son iguales");
-        } else if (n1 < n2 ) {
-            System.out.println("El numero mayor es:" + n2);
-        }
-        else {
-            System.out.println("El numero mayor es:" + n1);
-        }
     }
 }
